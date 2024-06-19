ad1638c459df079d49354b1542a1e8f24954636a
AryanAroraCCTB <aryan.arora@canctb.ca>
2024-06-18 20:34:22 -0700

    Basics

---
+++ basics.py
@@ -0,0 +1,58 @@
+# Variables
+x = 10 
+y = 10
+z = 11
+a = 5
+# print(x)
+
+# Casting Functions
+x = 3 # x is an INTEGER
+y = str(x) 
+# print('x is a: ', type(x))
+# print('y is a: ', type(y))
+
+y = int('3')
+
+y = int('12345678')
+# print('y is a: ', type(y))
+
+# y = int('25.12')
+
+y = int(25.12)
+# print('y is a: ', type(y), y)
+
+z = float(1)
+y = float('1')
+# print('z is a: ', type(z), z)
+
+
+a = bool('anything') # true for anything is NOT empty, false for 0 or empty
+a = bool(123)
+# print('a is a: ', type(a), a)
+
+b = bool([]) # 
+# print('b is a: ', type(b), b)
+
+c = bool('') # 
+# print('c is a: ', type(c), c)
+
+c = bool(print('')) #
+# print('c is a: ', type(c), c) 
+
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+print(1,2,3,4)
+# 1234
+
+# vs
+
+# 1
+# 2
+# 3
+# 4
+
+# TODO: WRITE BETTER COMMENTS PLEASE
+# FIXME: FIX THE MATH HERE
